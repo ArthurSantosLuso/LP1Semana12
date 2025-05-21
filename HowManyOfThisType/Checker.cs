e011efe6d5bcdb3e6137fa45a6d213d5b211c0b1
Arthur Santos <arthurluiz.developer@gmail.com>
2025-05-21 14:23:09 +0100

    Exercicio 1 iniciado. Class static Checker criada.

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowManyOfThisType
+{
+    static class Checker
+    {
+        public static int HowManyOfType<T>(IEnumerable<object> items)
+        {
+            int cntType = 0;
+
+            foreach(object o in items)
+            {
+                if (o is T)
+                {
+                    cntType++;
+                }
+            }
+            return cntType;
+        }
+    }
+}
---
+++ HowManyOfThisType/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HowManyOfThisType
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //object[] test = new object[3];
+            //test[0] = 2;
+            //test[1] = "aaa";
+            //test[2] = 100;
+
+            //Console.WriteLine(Checker.HowManyOfType<int>(test));
+        }
+    }
+}
