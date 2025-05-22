486a4953b14851b931f33e2d62f56f587f7f5fe8
Arthur Santos <arthurluiz.developer@gmail.com>
2025-05-22 21:57:18 +0100

    Exercicio 3 inicializado. Class generica Guarda3 adicionada

---
+++ MyGenericClass/Guarda3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGenericClass
+{
+    public class Guarda3<T>
+    {
+    }
+}
