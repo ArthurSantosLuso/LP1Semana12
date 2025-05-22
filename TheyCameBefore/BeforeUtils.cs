903862914743e2de0c8a747be122dc1bbc3c363d
Arthur Santos <arthurluiz.developer@gmail.com>
2025-05-22 21:51:06 +0100

    Classe BeforeUtils implementada

---
+++ TheyCameBefore/BeforeUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheyCameBefore
+{
+    static class BeforeUtils
+    {
+        public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, T limit) 
+            where T : struct, IComparable<T>
+        {
+            foreach (T item in items)
+            {
+                if (item.CompareTo(limit) < 0)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
